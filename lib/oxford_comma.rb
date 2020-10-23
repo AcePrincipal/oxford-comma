01088532752ad3256fe87bb4bd01b92097ebc677
Alexander <acepetrov96@gmail.com>
2020-10-22 19:04:26 -0500

    commit tree a9a35b43be2273392b36bfdef634bf03f3df3a78

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        counter = 0
+        arr = []
+        while counter < array.length-1
+            arr << (array[counter] + ", ")
+            counter += 1
+        end
+        arr << ("and " + array[counter])
+        return arr.join
+    end
 end
